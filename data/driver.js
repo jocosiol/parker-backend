d34914d0f6b89c8fa395ceff1d8942a5cc240294
jocosiol <jocosiol@gmail.com>
2022-01-12 16:56:19 +0200

    PUT /parking

--- data/driver.js
+++ data/driver.js
@@ -1,4 +1,15 @@
 const { query } = require("../lib/db");
 const SQL = require("@nearform/sql");
 
-module.exports = {  };
+
+  async function updateParkingById(parkingId) {
+    try {
+      const sql = SQL`UPDATE parking SET available=false WHERE id=${parkingId}`;
+      const editeAvailableParking = await query(sql);
+      return 
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+module.exports = { updateParkingById };
